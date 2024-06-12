d893ed33f9503e4ae86459ad6b1cdaf5ebf309fb
Makarem12 <makaremabososain@gmail.com>
2024-06-12 11:30:08 +0300

    the first attempt

---
+++ api/questions.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        msg = "questions generator"
+        self.wfile.write(msg.encode())
+
+        return
